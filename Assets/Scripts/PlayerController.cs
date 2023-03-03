a974bddcc0d1252fdb2c152ee847f127fe0b93d0
MaxMorfiX <offmax.me@gmail.com>
2023-03-03 13:54:39 +0100

    сreated empty unity project (almost empty)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControle : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     //Установка компонента для твердых тел
     private Rigidbody2D rb;
